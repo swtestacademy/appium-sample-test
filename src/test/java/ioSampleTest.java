a274f7189cc197f4dd170ab4f0ab47d681ab5d2d
swtestacademy <Onur1982+>
2019-03-16 20:13:55 +0400

    latest updates.

--- src/test/java/ioSampleTest.java
+++ src/test/java/ioSampleTest.java
@@ -1,7 +1,7 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,12 +9,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class ioSampleTest {
 
-    public AppiumDriver<MobileElement> driver;
+    public AndroidDriver<MobileElement> driver;
     public WebDriverWait wait;
 
     //Elements
@@ -24,15 +26,15 @@ public class ioSampleTest {
     @BeforeMethod
     public void setup () throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Galaxy Nexut API 24");
-        caps.setCapability("udid", "WUJ01N4RQ3"); //DeviceId from "adb devices" command
+        caps.setCapability("deviceName", "Galaxy Nexus API 24");
+        caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "7.0");
         caps.setCapability("skipUnlock","true");
         caps.setCapability("appPackage", "com.isinolsun.app");
         caps.setCapability("appActivity","com.isinolsun.app.activities.SplashActivity");
         caps.setCapability("noReset","false");
-        driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"),caps);
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -43,11 +45,7 @@ public class ioSampleTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated
                 (By.id("com.isinolsun.app:id/animation_view"))).click();
 
-        //Click and pass Tutorial
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/btn_skip"))).click();
-
-        //Click to "Is Ariyorum" Button
+        //Click I am searching a job
         wait.until(ExpectedConditions.visibilityOfElementLocated
                 (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
 
@@ -57,10 +55,8 @@ public class ioSampleTest {
             driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
         }
 
-        //Click second job
         wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath(secondNewJob))).click();
-        
+                (By.xpath("//android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout")));
     }
 
     @AfterMethod
