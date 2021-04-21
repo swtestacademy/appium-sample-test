f600884d4d8dc6e48ba7617fc3e2fb6832ee760f
Onur Baskirt <onur.baskirt@dnata.com>
2021-04-21 12:34:31 +0400

    Latest updates for Android 11 tests.

--- src/test/java/ioSampleTest.java
+++ src/test/java/ioSampleTest.java
@@ -1,15 +1,15 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ioSampleTest {
 
@@ -17,16 +17,18 @@ public class ioSampleTest {
     public WebDriverWait wait;
 
     //Elements
-    String secondNewJob = "//android.widget.FrameLayout[2]/android.widget.LinearLayout/" +
-            "android.widget.RelativeLayout/android.widget.ImageView";
+    By jobsBy = By.id("com.isinolsun.app:id/rootRelativeView");
+
+
+///hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup[2]/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.RelativeLayout
 
     @BeforeMethod
     public void setup () throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Galaxy Nexus API 24");
+        caps.setCapability("deviceName", "Pixel XL API 30");
         caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "7.0");
+        caps.setCapability("platformVersion", "11.0");
         caps.setCapability("skipUnlock","true");
         caps.setCapability("appPackage", "com.isinolsun.app");
         caps.setCapability("appActivity","com.isinolsun.app.activities.SplashActivity");
@@ -34,7 +36,6 @@ public class ioSampleTest {
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
         wait = new WebDriverWait(driver, 10);
     }
-
 
     @Test
     public void basicTest () throws InterruptedException {
@@ -46,14 +47,17 @@ public class ioSampleTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated
                 (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
 
-
         //Notification Allow
-        if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size()>0) {
-            driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
+        if (driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).isDisplayed()) {
+            driver.findElements(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).get(0).click();
         }
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath(secondNewJob)));
+        //Click Second Job
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(jobsBy)).get(1).click();
+
+        //Do a simple assertion
+        String jobDetail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.isinolsun.app:id/toolbarTitle"))).getText();
+        Assert.assertTrue(jobDetail.toLowerCase().contains("detay"));
     }
 
     @AfterMethod
