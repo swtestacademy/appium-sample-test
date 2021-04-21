1cbbdcbdffe84d03670bcee746e9ecd847561beb
Onur Baskirt <onur.baskirt@dnata.com>
2021-04-21 13:19:00 +0400

    Some improvements done.

--- src/test/java/ioSampleTest.java
+++ src/test/java/ioSampleTest.java
@@ -14,54 +14,53 @@ import org.testng.annotations.Test;
 public class ioSampleTest {
 
     public AndroidDriver<MobileElement> driver;
-    public WebDriverWait wait;
+    public WebDriverWait                wait;
 
-    //Elements
-    By jobsBy = By.id("com.isinolsun.app:id/rootRelativeView");
-
-
-///hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup[2]/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.RelativeLayout
-
+    //Elements By
+    By jobsBy           = By.id("com.isinolsun.app:id/rootRelativeView");
+    By allowWhenUsingBy = By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button");
+    By searchingJobBy   = By.id("com.isinolsun.app:id/bluecollar_type_button");
+    By animationBy      = By.id("com.isinolsun.app:id/animation_view");
+    By toolBarTitleBy   = By.id("com.isinolsun.app:id/toolbarTitle");
+    
     @BeforeMethod
-    public void setup () throws MalformedURLException {
+    public void setup() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("deviceName", "Pixel XL API 30");
         caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "11.0");
-        caps.setCapability("skipUnlock","true");
+        caps.setCapability("skipUnlock", "true");
         caps.setCapability("appPackage", "com.isinolsun.app");
-        caps.setCapability("appActivity","com.isinolsun.app.activities.SplashActivity");
-        caps.setCapability("noReset","false");
-        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+        caps.setCapability("appActivity", "com.isinolsun.app.activities.SplashActivity");
+        caps.setCapability("noReset", "false");
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void basicTest () throws InterruptedException {
+    public void basicTest() throws InterruptedException {
         //Click and pass Splash
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/animation_view"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(animationBy)).click();
 
         //Click I am searching a job
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchingJobBy)).click();
 
         //Notification Allow
-        if (driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).isDisplayed()) {
-            driver.findElements(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).get(0).click();
+        if (wait.until(ExpectedConditions.visibilityOfElementLocated(allowWhenUsingBy)).isDisplayed()) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(allowWhenUsingBy)).click();
         }
 
         //Click Second Job
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(jobsBy)).get(1).click();
 
         //Do a simple assertion
-        String jobDetail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.isinolsun.app:id/toolbarTitle"))).getText();
-        Assert.assertTrue(jobDetail.toLowerCase().contains("detay"));
+        String toolBarTitleStr = wait.until(ExpectedConditions.visibilityOfElementLocated(toolBarTitleBy)).getText();
+        Assert.assertTrue(toolBarTitleStr.toLowerCase().contains("detay"));
     }
 
     @AfterMethod
-    public void teardown(){
+    public void teardown() {
         driver.quit();
     }
 }
